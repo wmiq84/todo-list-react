5ae0b99f670defbdedecdc1a55e91043001bcf8d
wmiq84 <michaelwang3489@gmail.com>
2024-09-12 15:24:29 -0700

    Allow form input to show user inputs and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import Todo from "./components/Todo";
-import FilterButton from "./components/FilterButton";
-import Form from "./components/Form";
+import Todo from './components/Todo';
+import FilterButton from './components/FilterButton';
+import Form from './components/Form';
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => (
-    <Todo 
-      id={task.id} 
-      name={task.name} 
-      completed={task.completed}
-      key={task.id}
-    />
-  ));
+  function addTask(name) {
+    alert(name);
+  }
 
-  return (
-    <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <Form />
-      <div className="filters btn-group stack-exception">
-      <FilterButton />
-      <FilterButton />
-      <FilterButton />
-      </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
-        {taskList}
+	const taskList = props.tasks?.map((task) => (
+		<Todo
+			id={task.id}
+			name={task.name}
+			completed={task.completed}
+			key={task.id}
+		/>
+	));
 
-      </ul>
-    </div>
-  );
+	return (
+		<div className="todoapp stack-large">
+			<h1>TodoMatic</h1>
+			<Form addTask={addTask}/>
+			<div className="filters btn-group stack-exception">
+				<FilterButton />
+				<FilterButton />
+				<FilterButton />
+			</div>
+			<h2 id="list-heading">3 tasks remaining</h2>
+			<ul
+				role="list"
+				className="todo-list stack-large stack-exception"
+				aria-labelledby="list-heading"
+			>
+				{taskList}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
